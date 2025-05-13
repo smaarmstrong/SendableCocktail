824c7d18f050f1f976ca5796ff3b9fdd0f5a65b6
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2025-05-13 20:16:58 +0100

    create project

---
+++ SendableCocktail/SendableCocktail/SendableCocktailApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendableCocktailApp.swift
+//  SendableCocktail
+//
+//  Created by Sean Armstrong on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SendableCocktailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
